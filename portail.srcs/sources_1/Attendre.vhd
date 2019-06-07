fafdac8f2c2123f7b0b75c11bb04c184bd16e69a
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-06-07 07:34:20 +0000

    Update Attendre.vhd

--- portail.srcs/sources_1/Attendre.vhd
+++ portail.srcs/sources_1/Attendre.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: ENSEA
--- Engineer: Alban Benmouffek, SalomÈ Wattiaux, Marco Guzzon
+-- Engineer: Alban Benmouffek, Salom√© Wattiaux, Marco Guzzon
 -- 
 -- Create Date: 25.02.2019 08:52:41
 -- Design Name: 
@@ -9,13 +9,17 @@
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: ce module permet de faire une temporisation pendant un certain nombre de secondes.
---  Le nombre de secondes est dÈfini par Duree.
---  Fonctionnement: si la temporisation est en cours, alors Attente vaut '1'. 
---          Tant que le temps n'est pas ÈcoulÈ, FinAttente vaut '0'.
---          DËs que le temps est ÈcoulÈ, FinAttente vaut '1'.
---          FinAttente ne repasse ‡ '0' que si Attente repasse ‡ '0'
---  UtilitÈ: rester dans un Ètat pendant un certain nombre de secondes (dans une machine ‡ Ètat)
+--  Le nombre de secondes est d√©fini par Duree (variable d√©finie dans generic).
+--  Fonctionnement: 
+--          Etat initial: FinAttente √† '0'
+--          Si la temporisation est en cours, alors l'entr√©e Attente vaut '1'. 
+--          Tant que le temps n'est pas √©coul√©, la sortie FinAttente vaut '0'.
+--          D√®s que le temps est √©coul√©, la sortie FinAttente vaut '1'.
+--          La sortie FinAttente ne repasse √† '0' que si l'entr√©e Attente repasse √† '0'
+--  Utilit√©: rester dans un √©tat pendant un certain nombre de secondes (dans une machine √† √©tat)
 -- 
+--  L'entr√©e ForceReset permet de remettre le compteur de temps √† z√©ro
+--
 -- Dependencies: 
 --  Tick_Ns (Tick_Ns.vhd)
 --  DFM (DFM.vhd)
@@ -32,28 +36,29 @@ entity Attendre is
     Port ( 
         --ENTREES DIVERSES:
         CLK : in STD_LOGIC; --Horloge
-        ForceReset : in STD_LOGIC; --Reset forcÈ
+        ForceReset : in STD_LOGIC; --Reset forc√©
         Attente : in STD_LOGIC; --  Attente = '1' <=> une temporisation est en cours
         
         --SORTIES DIVERSES:
-        FinAttente : out STD_LOGIC -- FinAttente = '1' <=> la temporisation est terminÈe
+        FinAttente : out STD_LOGIC -- FinAttente = '1' <=> la temporisation est termin√©e
     );
 end Attendre;
 
 architecture Behavioral of Attendre is
-    signal Reset: STD_LOGIC; --front montant du signal Attente (dÈbut de la temporisation)
-    signal Tick: STD_LOGIC; --Vaut '1' pendant une pÈriode d'horloge toutes les Duree seconde
-    signal fin : STD_LOGIC; --Vaut '1' pendant une pÈriode d'horloge ‡ la fin de la temporisation
+    signal Reset: STD_LOGIC; --front montant du signal Attente (d√©but de la temporisation), joue le r√¥le de synchronisation
+    signal Tick: STD_LOGIC; --Vaut '1' pendant une p√©riode d'horloge toutes les Duree seconde
+    signal fin : STD_LOGIC; --Vaut '1' pendant une p√©riode d'horloge √† la fin de la temporisation
     signal count : integer range 0 to Duree := 0;
 --    signal FinAttenteSignal : STD_LOGIC := '0';
 begin
+    
     waiter: 
         entity work.TICK_Ns 
         generic map (
-            N => 2 --Un 'Tick' toutes les seconde
+            N => 2 --Un 'Tick' toutes les secondes
         ) 
         port map(
-            --EntrÈes:
+            --Entr√©es:
             CLK => CLK, 
             Reset => Reset,
             --Sorties: 
@@ -63,7 +68,7 @@ begin
     dfm: 
         entity work.DFM 
         port map(
-            --EntrÈes:
+            --Entr√©es:
             CLK => CLK, 
             entree => Attente,
             --Sorties: 
@@ -72,10 +77,10 @@ begin
     
     process(clk) begin
         if rising_edge(clk) then
-            if Attente = '0' or Reset = '1' or ForceReset = '1' then
+            if Attente = '0' or Reset = '1' or ForceReset = '1' then --Il faut mettre le compteur √† z√©ro
                 count <= 0;
             elsif Tick = '1' and Attente = '1' then
-                if count = Duree then --on a fini de compter
+                if count = Duree then --On a fini de compter
                     count <= 0;
                 else
                     count <= count+1;
@@ -85,16 +90,6 @@ begin
     end process;
     
     fin <= '1' when count = Duree and Attente = '1' else '0';
-    
---    process(fin, clk) begin
---        if fin = '1' then
---            FinAttenteSignal <= '1';
---        elsif fin = '0' and Attente = '0' then
---            FinAttenteSignal <= '0';
---        elsif fin = '0' and Attente = '1' then
---            FinAttenteSignal <= FinAttenteSignal;
---        end if;
---    end process;
     
     FinAttente <= fin;
     
