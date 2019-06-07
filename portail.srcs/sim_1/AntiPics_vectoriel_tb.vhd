97156971aa5215d3224b582c6f98ab215dc31c08
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-06-07 07:21:44 +0000

    Update AntiPics_vectoriel_tb.vhd

--- portail.srcs/sim_1/AntiPics_vectoriel_tb.vhd
+++ portail.srcs/sim_1/AntiPics_vectoriel_tb.vhd
@@ -1,35 +1,22 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: ENSEA
+-- Engineer: Alban Benmouffek, Salomé Wattiaux, Marco Guzzon
 -- 
 -- Create Date: 28.02.2019 17:08:25
 -- Design Name: 
 -- Module Name: AntiPics_vectoriel_tb - Behavioral
--- Project Name: 
+-- Project Name: Portail
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
 -- 
 -- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- AntiPics_vectoriel (AntiPics_vectoriel.vhd)
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity AntiPics_vectoriel_tb is
 --  Port ( );
