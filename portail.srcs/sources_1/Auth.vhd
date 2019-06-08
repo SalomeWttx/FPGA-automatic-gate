aefd0b09e86e2f1b94e9eaa32993d58f7f686d2f
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-06-08 13:17:27 +0000

    Update Auth.vhd

--- portail.srcs/sources_1/Auth.vhd
+++ portail.srcs/sources_1/Auth.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: ENSEA
--- Engineer: Alban Benmouffek, Salomé Wattiaux, Marco Guzzon
+-- Engineer: Alban Benmouffek, SalomÃ© Wattiaux, Marco Guzzon
 -- 
 -- Create Date: 25.02.2019 16:31:37
 -- Design Name: 
@@ -8,11 +8,13 @@
 -- Project Name: Portail
 -- Target Devices: 
 -- Tool Versions: 
--- Description: authentifie le propriétaire, et envoie les signaux permettant de contrôler le portail
+-- Description: authentifie le propriÃ©taire, et envoie les signaux permettant de contrÃ´ler le portail
 --
 -- Dependencies: 
 --  GestionCode (GestionCode.vhd)
 --  GestionPortail (GestionPortail.vhd)
+--  GestionTelecommunications (GestionTelecommunications.vhd)
+--  GestionBoutons (GestionBoutons.vhd)
 ----------------------------------------------------------------------------------
 
 library IEEE;
@@ -21,7 +23,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Auth is
     generic (
-        DigicodeOvertime : integer := 10 --Temps d'inactivité max du digicode (en secondes)
+        DigicodeOvertime : integer := 10 --Temps d'inactivitÃ© max du digicode (en secondes)
     );
     Port ( 
         --ENTREES DIVERSES:
@@ -31,28 +33,29 @@ entity Auth is
         boutonOuvrir : in STD_LOGIC; -- vaut '1' si on veut ouvrir le portail
         boutonStop : in STD_LOGIC; -- vaut '1' si on veut fermer le portail
         boutonFermer : in STD_LOGIC; -- vaut '1' si on veut fermer le portail
-        boutonChange : in STD_LOGIC; -- vaut '1' si on veut changer l'état du portail
+        boutonChange : in STD_LOGIC; -- vaut '1' si on veut changer l'Ã©tat du portail
         
         --Info venant du digicode:
-        numeroTouche : in STD_LOGIC_VECTOR (3 downto 0); --numéro de la touche pressée
-        toucheDetectee : in STD_LOGIC; --vaut '1' pendant une période d'horloge si un nouvel appui est détecté
+        numeroTouche : in STD_LOGIC_VECTOR (3 downto 0); --numÃ©ro de la touche pressÃ©e
+        toucheDetectee : in STD_LOGIC; --vaut '1' pendant une pÃ©riode d'horloge si un nouvel appui est dÃ©tectÃ©
         
         --SORTIES DIVERSES:
-        --Contrôle du portail
-        ForceOuverture : out STD_LOGIC; --Demande d'ouverture du portail (activé que pendant une période d'horloge)
-        ForceFermeture : out STD_LOGIC; --Demande de fermeture du portail (activé que pendant une période d'horloge)
-        ControlSignal : out STD_LOGIC; --Demande de changement d'état du portail (comme si on avait une télécommande à 1 bouton) (activé que pendant une période d'horloge)
-        Stop : out STD_LOGIC; --Demande d'arrêt du portail
+        --ContrÃ´le du portail
+        ForceOuverture : out STD_LOGIC; --Demande d'ouverture du portail (activÃ© que pendant une pÃ©riode d'horloge)
+        ForceFermeture : out STD_LOGIC; --Demande de fermeture du portail (activÃ© que pendant une pÃ©riode d'horloge)
+        ControlSignal : out STD_LOGIC; --Demande de changement d'Ã©tat du portail (comme si on avait une tÃ©lÃ©commande Ã  1 bouton) (activÃ© que pendant une pÃ©riode d'horloge)
+        Stop : out STD_LOGIC; --Demande d'arrÃªt du portail
 
         
         --haut parleur
-        NombreBip : out STD_LOGIC_VECTOR(2 downto 0); --Nombre de répétitions. MAX 7 REPETITIONS!
-        DureeBip : out STD_LOGIC_VECTOR(6 downto 0);--Durée de chaque bip, ainsi que entre les bip, en dixieme de seconde MAX 12,7s !
+        NombreBip : out STD_LOGIC_VECTOR(2 downto 0); --Nombre de rÃ©pÃ©titions. MAX 7 REPETITIONS!
+        DureeBip : out STD_LOGIC_VECTOR(6 downto 0);--DurÃ©e de chaque bip, ainsi que entre les bip, en dixieme de seconde MAX 12,7s !
         TonBip : out STD_LOGIC; -- ..=1 <=> bip aigu!
-        DataBip : out STD_LOGIC;--Vaut '1' pendant une période d'horloge quand des nouvelles données sont envoyées
+        DataBip : out STD_LOGIC;--Vaut '1' pendant une pÃ©riode d'horloge quand des nouvelles donnÃ©es sont envoyÃ©es
+        
         --LED
-        retroEclairageDigicode : out STD_LOGIC;
-        LEDdigicode : out STD_LOGIC_VECTOR(4 downto 0)
+        retroEclairageDigicode : out STD_LOGIC; --Eclairage du calvier si on est en train de l'utiliser
+        LEDdigicode : out STD_LOGIC_VECTOR(4 downto 0) --5 LED indiquant quelle fonction du digicode est en train d'Ãªtre utilisÃ©e (par exemple changer le code, ouvrir le portail...)
     );
 end Auth;
 
@@ -73,7 +76,7 @@ begin
             TempsAttenteMax => DigicodeOvertime
         )
         port map(
-            --Entrées:
+            --EntrÃ©es:
             CLK => CLK,
             numeroTouche => numeroTouche,
             toucheDetectee => toucheDetectee,
@@ -90,7 +93,7 @@ begin
     telecommandes:
         entity work.GestionTelecommunications
         port map (
-            --Entrées:
+            --EntrÃ©es:
             CLK => CLK,
             --Sorties:
             fonction => telecommandes_gestion_fonctionTelecommande
@@ -99,7 +102,7 @@ begin
     boutons:
         entity work.GestionBoutons
         port map (
-            --Entrées:
+            --EntrÃ©es:
             CLK => CLK,
             boutonOuvrir => boutonOuvrir,
             boutonFermer => boutonFermer,
@@ -112,7 +115,7 @@ begin
     gestion:
         entity work.GestionPortail
         port map (
-            --Entrées:
+            --EntrÃ©es:
             CLK => CLK,
             fonctionDigicode => code_gestion_fonctionDigicode,
             fonctionTelecommande => telecommandes_gestion_fonctionTelecommande,
