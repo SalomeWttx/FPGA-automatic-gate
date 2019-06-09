81161cec9e516635ee0ad48b47d7587f6f1250ae
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-06-09 07:59:41 +0000

    Update GestionCode.vhd

--- portail.srcs/sources_1/GestionCode.vhd
+++ portail.srcs/sources_1/GestionCode.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: ENSEA
--- Engineer: Alban Benmouffek, SalomÈ Wattiaux, Marco Guzzon 
+-- Engineer: Alban Benmouffek, Salom√© Wattiaux, Marco Guzzon 
 -- 
 -- Create Date: 01.03.2019 19:05:30
 -- Design Name: 
@@ -8,25 +8,25 @@
 -- Project Name: Portail
 -- Target Devices: 
 -- Tool Versions: 
--- Description: gËre la saisie du code, la modification du code, l'activation des diffÈrentes fonctions accessibles via le digicode.
---  Si le code est erronÈ moins de 10 fois de suite, il y a une temporisation de 1s avant de pouvoir rÈutiliser le digicode.
---  Si le code est erronÈ plus de 10 fois de suite, il y a une temporisation de 10s avant de pouvoir rÈutiliser de digicode.
+-- Description: g√®re la saisie du code, la modification du code, l'activation des diff√©rentes fonctions accessibles via le digicode.
+--  Si le code est erron√© moins de 10 fois de suite, il y a une temporisation de 1s avant de pouvoir r√©utiliser le digicode.
+--  Si le code est erron√© plus de 10 fois de suite, il y a une temporisation de 10s avant de pouvoir r√©utiliser de digicode.
 --
---  Le code peut faire n'importe quelle taille, fixÈe ‡ l'avance dans ce fichier (par dÈfaut 6 chiffres de 0 ‡ 9). Ce paramËtre est dÈfini par taille_code.
+--  Le code peut faire n'importe quelle taille, fix√©e √† l'avance dans ce fichier (par d√©faut 6 chiffres de 0 √† 9). Ce param√®tre est d√©fini par taille_code.
 --
---  Avec un code ‡ 6 chiffres, une attaque brute force visant ‡ trouver le code nÈcessitera au plus 9999910 secondes, c'est-‡-dire environ 115 jours.
+--  Avec un code √† 6 chiffres, une attaque brute force visant √† trouver le code n√©cessitera au plus 9999910 secondes, c'est-√†-dire environ 115 jours.
 --
--- Le code est stockÈ en clair dans des bascules du FPGA. Ce n'est pas pertinent de Hasher ce code car l'Ètat interne du FPGA est trËs difficile d'accËs (comparÈ aux fils contrÙlant le moteur).
+-- Le code est stock√© en clair dans des bascules du FPGA. Ce n'est pas pertinent de Hasher ce code car l'√©tat interne du FPGA est tr√®s difficile d'acc√®s (compar√© aux fils contr√¥lant le moteur).
 --
--- Sur le digicode, les touches de 10 ‡ 15 correspondent ‡ des fonctions spÈciales. 
+-- Sur le digicode, les touches de 10 √† 15 correspondent √† des fonctions sp√©ciales. 
 --touche      colonnes(0)     colonnes(1)     colonnes(2)     colonnes(3)
 --lignes(0)       1               2               3              10
 --lignes(1)       4               5               6              11
 --lignes(2)       7               8               9              12
 --lignes(3)       15              0              14              13
 --
--- Pour utiliser une fonction spÈciale, il faut cliquer sur la touche correspondante puis saisir le code.
--- La fonction choisie est dÈcrite par la variable fonction_activee.
+-- Pour utiliser une fonction sp√©ciale, il faut cliquer sur la touche correspondante puis saisir le code.
+-- La fonction choisie est d√©crite par la variable fonction_activee.
 -- 10 : Ouvrir le Portail
 -- 11 : Modifier le code
 -- 15 : Annuler
@@ -35,6 +35,7 @@
 -- 0 : rien
 --
 -- Dependencies: 
+--  Attendre (Attendre.vhd)
 --  DFM (DFM.vhd)
 --  TICK_Ns (TICK_Ns.vhd)
 ----------------------------------------------------------------------------------
@@ -45,26 +46,26 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity GestionCode is
     generic (
-        taille_code : integer := 6;  --taille du code
-        TempsAttenteMax : integer := 10  --nombre de secondes ‡ attendre avant l'annulation automatique
+        taille_code : integer := 6;  --taille du code (nombre de chiffres)
+        TempsAttenteMax : integer := 10  --nombre de secondes √† attendre avant l'annulation automatique
     );
     Port ( 
-        CLK : in STD_LOGIC;
+        CLK : in STD_LOGIC; --Horloge
         
         --Digicode complet:
-        numeroTouche : in STD_LOGIC_VECTOR (3 downto 0); --NumÈro de la touhe dÈtectÈe (dÈtails dans la description)
-        toucheDetectee : in STD_LOGIC; --Vaut '1' pendant une pÈriode d'horloge quand une nouvelle touche est dÈtectÈe
+        numeroTouche : in STD_LOGIC_VECTOR (3 downto 0); --Num√©ro de la touhe d√©tect√©e (d√©tails dans la description)
+        toucheDetectee : in STD_LOGIC; --Vaut '1' pendant une p√©riode d'horloge quand une nouvelle touche est d√©tect√©e
         
-        LEDdigicode : out STD_LOGIC_VECTOR(4 downto 0);
-        retroEclairage : out STD_LOGIC;
+        LEDdigicode : out STD_LOGIC_VECTOR(4 downto 0); --LED indiquant quelle fonction du digicode est utilis√©e (voir dans la description la liste des fonctions sp√©ciales)
+        retroEclairage : out STD_LOGIC; --LED allum√©e si le digicode est en cours d'utilisation
         
         --haut parleur
-        NombreBip : out STD_LOGIC_VECTOR(2 downto 0);--Nombre de rÈpÈtitions. MAX 7 REPETITIONS!
-        DureeBip : out STD_LOGIC_VECTOR(6 downto 0);--DurÈe de chaque bip, ainsi que entre les bip, en dixieme de seconde MAX 12,7s !
+        NombreBip : out STD_LOGIC_VECTOR(2 downto 0);--Nombre de r√©p√©titions. MAX 7 REPETITIONS!
+        DureeBip : out STD_LOGIC_VECTOR(6 downto 0);--Dur√©e de chaque bip, ainsi que entre les bip, en dixieme de seconde MAX 12,7s !
         TonBip : out STD_LOGIC; -- ..=1 <=> bip aigu!
-        DataBip : out STD_LOGIC;
+        DataBip : out STD_LOGIC; --Vaut 1 pendant 1 p√©riode d'horloge si on est en train de param√™trer un nouveau bip (plus de d√©tails dans HautParleur.vhd)
         
-        fonction : out STD_LOGIC_VECTOR(2 downto 0)
+        fonction : out STD_LOGIC_VECTOR(2 downto 0) --Ordre donn√© au portail
         --ORDRES:
         --rien : "000"
         --changer : "011"
@@ -75,43 +76,46 @@ entity GestionCode is
 end GestionCode;
 
 architecture Behavioral of GestionCode is
-    --pour la saisie du code: (de 1 ‡ 6 chiffres)
+    --pour la saisie du code: (de 1 √† 6 chiffres)
     type LIST is array (taille_code-1 downto 0) of integer range 0 to 9;
-    signal code_saisi : LIST; --Code en train d'Ítre saisi
+    signal code_saisi : LIST; --Code en train d'√™tre saisi
     signal chiffre_saisi : integer range 0 to taille_code := 0; --Chiffre (du code) que l'on a saisi en dernier (0 si on n'a rien saisi)
-    signal code : LIST;
-    signal fonction_activee : integer range 0 to 15 := 0;--Si aucune fonction n'est active : 0
-    signal nombreEchec : integer range 0 to 10 := 0;
+    signal code : LIST; --Code enregistr√© en clair
+    signal fonction_activee : integer range 0 to 15 := 0;--Fonction sp√©ciale du digicode qui est en train d'√™tre utilis√©e (plus de d√©tails dans la description). Si aucune fonction n'est active : 0
+    signal nombreEchec : integer range 0 to 10 := 0; --Nombre d'√©checs cons√©cutifs
     
     --Si l'utilisation est en cours
-    signal utilisation : STD_LOGIC := '0'; --Si le digicode est en train d'Ítre utilisÈ
-    signal overtimed : STD_LOGIC := '0'; --Si l'utilisateur met trop de temps ‡ appuyer sur les touches
+    signal utilisation : STD_LOGIC := '0'; --Vaut '1' si le digicode est en train d'√™tre utilis√©
+    signal overtimed : STD_LOGIC := '0'; --Si l'utilisateur met trop de temps √† appuyer sur les touches
     
     --pour changer le code :
-    signal code_existe : STD_LOGIC := '0';
-    signal nouveau_code : LIST;
+    signal code_existe : STD_LOGIC := '0'; --Vaut '0' si le code n'a pas encore √©t√© cr√©√© (√† l'allumage du FPGA)
+    signal nouveau_code : LIST; --Futur code (n√©cessit√© de le stocker dans une autre variable pour pouvoir le comparer √† la confirmation, sans effacer le code pr√©c√©dent
     
-    signal fonctionDigicode : STD_LOGIC_VECTOR(2 downto 0) := "000";
+    signal fonctionDigicode : STD_LOGIC_VECTOR(2 downto 0) := "000"; 
+
+    --Pour l'haut-parleur (ces signaux seront directement envoy√©s sur leus sortie respective)
     signal DataBip_s : STD_LOGIC := '0';
     signal NombreBip_s : STD_LOGIC_VECTOR(2 downto 0) := "000";
     signal DureeBip_s : STD_LOGIC_VECTOR(6 downto 0) := "0000000";
     signal TonBip_s : STD_LOGIC := '0';
     
-    --pour les temporisations
-    signal temporisationClavier : STD_LOGIC := '0';
+    --pour les temporisations (temporisation = l'utilisateur s'est tromp√© de code donc, par s√©curit√©, le digicode se bloque pendant un instant)
+    signal temporisationClavier : STD_LOGIC := '0';  --Vaut '1' si une temporisation est en cours (1s par d√©faut)
+    signal temporisationClavierLongue : STD_LOGIC := '0'; --Vaut '1' si une longue temporisation est en cours (10s par d√©faut)
     signal debutTemporisation : STD_LOGIC := '0';
     signal finTemporisation : STD_LOGIC := '0';
     signal debutTemporisationLongue : STD_LOGIC := '0';
     signal finTemporisationLongue : STD_LOGIC := '0';
-    signal temporisationClavierLongue : STD_LOGIC := '0';
     
 begin
-    --pour les temporisations:
+    --Pour les temporisations: (c'est Tick1s et Tick10s qui disent si la temporisation est finie ou non)
     DFM1 : entity work.DFM port map(CLK => CLK, entree => temporisationClavier, front => debutTemporisation);
     Tick1s : entity work.Tick_Ns generic map (N => 2) port map (CLK => CLK, Reset => debutTemporisation, Tick => finTemporisation);
     DFM2 : entity work.DFM port map(CLK => CLK, entree => temporisationClavierLongue, front => debutTemporisationLongue);
     Tick10s : entity work.Tick_Ns generic map (N => 20) port map (CLK => CLK, Reset => debutTemporisationLongue, Tick => finTemporisationLongue);
     
+    --Pour dire si le clavier est en cours d'utilisation ou non (on compte le temps depuis le dernier signe de vie de l'utilisateur)
     attendre :
         entity work.Attendre
         generic map (
@@ -129,7 +133,9 @@ begin
     process(CLK) begin
         if rising_edge(CLK) then
             
-            if temporisationClavier = '1' or temporisationClavierLongue = '1' then
+            
+            --GESTION DES TEMPORISATIONS
+            if temporisationClavier = '1' or temporisationClavierLongue = '1' then --blocage du clavier
                 DataBip_s <= '0';
                 fonctionDigicode <= "000";
                 chiffre_saisi <= 0;
@@ -145,18 +151,24 @@ begin
                     temporisationClavierLongue <= temporisationClavierLongue;
                     temporisationClavier <= temporisationClavier;
                 end if;
-                
+                    
+                    
+            --SI AUCUN CODE N'EXISTE (au d√©marrage du FPGA) -> on demande √† l'utilisateur de saisir un code, pour cela on active la fonction sp√©ciale 1 (plus de d√©tails sur les fonctions sp√©ciales dans la description)
             elsif code_existe = '0' and NOT(fonction_activee = 1 or fonction_activee = 2) then
                 DataBip_s <= '0';
                 fonctionDigicode <= "000";
                 fonction_activee <= 1;
             
+
+            --SI L'UTILISATEUR FAIT LE MORT DEPUIS TROP LONGTEMPS
             elsif overtimed = '1' and code_existe = '1' then
                 DataBip_s <= '1';NombreBip_s <= "001";DureeBip_s <= "0000011";TonBip_s <= '0';  
                 fonctionDigicode <= "000";
                 chiffre_saisi <= 0;
                 fonction_activee <= 0;
             
+
+            --SI L'UTILISATEUR DONNE UN SIGNE DE VIE
             elsif toucheDetectee = '1' and temporisationClavier = '0' and temporisationClavierLongue = '0' then
                 fonctionDigicode <= "000";
                 DataBip_s <= '1';NombreBip_s <= "001";DureeBip_s <= "0000001";TonBip_s <= '1';
@@ -180,6 +192,8 @@ begin
                     fonction_activee <= TO_INTEGER(UNSIGNED(numeroTouche));
                 end if;
             
+                    
+            --SI L'UTILISATEUR A FINI DE SAISIR UN CODE
             elsif chiffre_saisi = taille_code then
                 chiffre_saisi <= 0;
                 
@@ -188,13 +202,13 @@ begin
                     nouveau_code <= code_saisi;
                     --NOUVEAU CODE ENTRE UNE FOIS:OK
                     DataBip_s <= '1';NombreBip_s <= "010";DureeBip_s <= "0000010";TonBip_s <= '1';
-                    fonction_activee <= 2; --(Ètape 1: retaper le nouveau code)
+                    fonction_activee <= 2; --(√©tape 1: retaper le nouveau code)
                     
                 elsif fonction_activee = 2 then
                     fonctionDigicode <= "000";
                     --NOUVEAU CODE ENTRE DEUX FOIS:OK
                     if code_saisi = nouveau_code then
-                        --Nouveau code ajoutÈ
+                        --Nouveau code ajout√©
                         DataBip_s <= '1';NombreBip_s <= "110";DureeBip_s <= "0000001";TonBip_s <= '1';
                         
                         code <= nouveau_code;
@@ -212,7 +226,7 @@ begin
                     
                     DataBip_s <= '1';NombreBip_s <= "010";DureeBip_s <= "0000010";TonBip_s <= '1';
                     
-                    --Changer l'Ètat du portail
+                    --Changer l'√©tat du portail
                     if fonction_activee = 0 then
                         fonctionDigicode <= "011";
                         fonction_activee <= 0;
@@ -222,7 +236,7 @@ begin
                         fonctionDigicode <= "001";
                         fonction_activee <= 0;
                         
-                    --Changer le code (Ètape 1: taper le nouveau code)
+                    --Changer le code (√©tape 1: taper le nouveau code)
                     elsif fonction_activee = 11 then
                         fonctionDigicode <= "000";
                         fonction_activee <= 1;
@@ -230,7 +244,7 @@ begin
                         fonctionDigicode <= "000";
                     end if;
                     
-
+                --Code erron√©!!
                 else
                     fonctionDigicode <= "000";
                     --CODE ERRONNE
@@ -244,10 +258,14 @@ begin
                         nombreEchec <= nombreEchec;
                     end if;
                 end if;
+                    
+             
+            --SI RIEN DE TOUT √áA:
             else
                 fonctionDigicode <= "000";
                 DataBip_s <= '0';
-            end if;
+                        
+            end if;--Enfin le "end if" !!!
         end if;
     end process;
     
