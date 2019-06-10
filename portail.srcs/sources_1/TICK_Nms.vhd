bb4f79daedc6b6e851380f2dd9e17b4b368072a4
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-06-10 07:44:49 +0000

    Update TICK_Nms.vhd

--- portail.srcs/sources_1/TICK_Nms.vhd
+++ portail.srcs/sources_1/TICK_Nms.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: Alban Benmouffek
+-- Company: ENSEA
+-- Engineer: Alban Benmouffek, Salomé Wattiaux, Marco Guzzon
 -- 
 -- Create Date: 25.02.2019 08:14:23
 -- Design Name: 
@@ -8,14 +8,9 @@
 -- Project Name: Portail
 -- Target Devices: 
 -- Tool Versions: 
--- Description: envoie un signal de une p�riode d'horloge toutes les N millisecondes. Reset permet de remettre a 0 (arret si Reset passe a '1', remise a zero des que Reset passe a '0')
+-- Description: envoie un signal de une période d'horloge toutes les N millisecondes. Reset permet de remettre a 0 (arret si Reset passe a '1', remise a zero des que Reset passe a '0')
 -- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
@@ -30,19 +25,19 @@ entity TICK_Nms is
     Port ( 
         CLK : in STD_LOGIC; --Horloge
         Reset: in STD_LOGIC; --signal de debut
-        Tick : out STD_LOGIC --Signal de p�riode N millisecondes
+        Tick : out STD_LOGIC --Signal de période N millisecondes
     );
 end TICK_Nms;
 
 architecture Behavioral of TICK_Nms is
-    constant div : integer := 100000*N;
-    signal count : integer range 0 to div := 0;
+    constant div : integer := 100000*N; --Nombre de fronts d'horloge que l'on doit compter
+    signal count : integer range 0 to div := 0; --Compteur
 begin
     process(CLK, Reset) begin
         if rising_edge(CLK) then
-            if count = div then
+            if count = div then --on a fini de compter
                 count <= 0;
-            elsif Reset = '1' then
+            elsif Reset = '1' then --remise à zéro
                 count <= 1;
             else
                 count <= count+1;
